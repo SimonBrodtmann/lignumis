b714b0f6fe52eb0ec57630b8d179366182301a25
Simon Brodtmann <github@accounts.brodtmann.info>
2025-01-07 10:21:54 +0100

    Add compatibility with hot metals

--- lignumis/data.lua
+++ lignumis/data.lua
@@ -2,3 +2,4 @@ require("__cf-lib__/util")
 
 require("prototypes/content/data")
 require("prototypes/integrations/data")
+require("prototypes/compatibility/data")
---
+++ lignumis/prototypes/compatibility/hot-metals.lua
@@ -0,0 +1,5 @@
+if not mods["hot-metals"] then
+    return
+end
+
+table.insert(HotMetals.items, { name = "gold-plate", iconFolder = "__lignumis__/graphics/icons/" })
