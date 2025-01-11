bf6d6fa23a5e072c39b054685b97d8c09d7bd909
Simon Brodtmann <github@accounts.brodtmann.info>
2025-01-11 15:12:47 +0100

    Set deep miner recipe ingredients

--- lignumis/prototypes/content/deep-miner.lua
+++ lignumis/prototypes/content/deep-miner.lua
@@ -12,11 +12,20 @@ local entity = DeepMiner.EntityBuilder:new():build({
 entity.resource_categories = { "deep-mining" }
 data:extend({ entity })
 
-DeepMiner.ItemBuilder:new():apply()
+DeepMiner.ItemBuilder:new()
+    :itemsPerRocket(1)
+    :apply({
+        subgroup = "extraction-machine",
+        order = "a[items]-d[deep-miner]"
+    })
 
 DeepMiner.RecipeBuilder:new()
     :ingredients({
-        { type = "item", name = "iron-plate", amount = 100 }
+        { type = "item", name = "processing-unit", amount = 100 },
+        { type = "item", name = "electric-engine-unit", amount = 100 },
+        { type = "item", name = "steel-plate", amount = 200 },
+        { type = "item", name = "concrete", amount = 50 }
+
     })
     :apply()
 
@@ -25,10 +34,10 @@ DeepMiner.TechnologyBuilder:new()
     :count(500)
     :ingredients({
         { "automation-science-pack", 1 },
-        { "logistic-science-pack", 1 },
-        { "chemical-science-pack", 1 },
+        { "logistic-science-pack",   1 },
+        { "chemical-science-pack",   1 },
         { "production-science-pack", 1 },
-        { "space-science-pack", 1 }
+        { "space-science-pack",      1 }
     })
     :time(60)
     :apply()
