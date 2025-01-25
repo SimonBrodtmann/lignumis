9c27c755e511da87de368a56a1509984f487c943
Simon Brodtmann <github@accounts.brodtmann.info>
2025-01-25 10:52:32 +0100

    Add mod support for science packs and custom labs

--- lignumis/data.lua
+++ lignumis/data.lua
@@ -1,10 +1,27 @@
 Lignumis = {
+    -- A technology having any of these science packs as ingredient will get wood and steam science packs added
+    science_pack_whitelist = { "utility-science-pack", "production-science-pack", "space-science-pack" },
+
+    -- A technology is skipped if it has any of these as ingredient
+    science_pack_blacklist = {
+        "wood-science-pack", "steam-science-pack", -- Lignumis
+        "nanite-science-pack", "quantum-science-pack", "ring-science-pack", -- Metal and Stars
+        "cerys-science-pack" -- Cerys
+    },
+
+    -- These technologies are skipped even if they are matches according to above lists
     science_blacklist = {
         "deep-miner",
         "logistic-system",
         "space-platform-thruster",
         "kovarex-enrichment-process",
         "astroponics"
+    },
+
+    -- Add wood and steam science packs to all labs' inputs except these
+    lab_blacklist = {
+        "wood-lab", -- Lignumis
+        "cerys-lab" -- Cerys
     }
 }
 
