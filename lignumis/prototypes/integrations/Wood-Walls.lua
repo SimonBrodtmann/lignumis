d33eb0a3190e07b9c7a368bd4e7e7dacb3368c7c
Simon Brodtmann <github@accounts.brodtmann.info>
2025-01-29 18:57:25 +0100

    Temporary fix for crash due to "Wood Walls and floors"

--- lignumis/prototypes/integrations/Wood-Walls.lua
+++ lignumis/prototypes/integrations/Wood-Walls.lua
@@ -1,3 +1,6 @@
 local recipe = data.raw.recipe["wooden-wall"]
 recipe.ingredients = { { type = "item", name = "lumber", amount = 4 } }
 recipe.results = { { type = "item", name = "wooden-wall", amount = 1 } }
+
+-- https://mods.factorio.com/mod/Wood-Walls/discussion/679915cbb8bf3c84c65db404
+data.raw.corpse["woodenwall-remnants"].icon = "__Wood-Walls__/graphics/wooden-wall/wooden-wall.png"
